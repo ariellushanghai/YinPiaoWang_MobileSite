957140d7e1ff444510b86076f83bf01a98e42a17
陆欢 <ariellushanghai@icloud.com>
2015-05-29 11:42:20 +0800

    For Fuck Sake

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 /**
  * Created by <陆欢 ariellushanghai@icloud.com>on 4/15/15.
  */
-var App = angular.module('App', ['ngRoute']);
-App.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+var App = angular.module('App', ['ngRoute', 'ngCookies']);
+App.config(['$routeProvider', '$locationProvider', '$httpProvider', function ($routeProvider, $locationProvider, $httpProvider) {
     $routeProvider.
         when('/', {
             templateUrl: 'partials/main.html',
@@ -32,34 +32,103 @@ App.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
             redirectTo: '/'
         });
     $locationProvider.html5Mode(true).hashPrefix('!');
+    $httpProvider.defaults.headers.common['Cache-Control'] = 'no-cache';
+    $httpProvider.defaults.withCredentials = true;
 }]);
+
+App.run(function ($rootScope) {
+    $rootScope.log = function (str) {
+        console.log(str);
+    }
+});
 //=======================   服务  =======================
 //登陆服务
-App.factory('loginService', ['$http', function ($http) {
-    var TEST_SERVER_BASE_URL = 'http://192.168.1.70/mobile/jsonp?url=';
+App.factory('loginService', ['$http', '$cookies', function ($http, $cookies) {
+    var TEST_SERVER_BASE_URL = 'http://192.168.1.70/mobile/jsonp';
+    var TEST_SERVER_BASE_URL_checkimage = 'http://test2.yinpiao.com/jsonp';
+    var CAPTCHA_SERVER_BASE_URL = '/verification/checkImage';
+
     return {
-        login: function (cred, sucess, error) {
-            var arg = encodeURIComponent("mobile/user/login/?name=" + cred.username + "&password=" + cred.password + "&checkcode=" + cred.checkcode);
-            console.log('url: ', TEST_SERVER_BASE_URL + arg + "&callback=JSON_CALLBACK");
+        fetchCaptcha: function (sucess, error) {
             $http({
                 method: "JSONP",
-                url: TEST_SERVER_BASE_URL + arg + "&callback=JSON_CALLBACK"
+                url: TEST_SERVER_BASE_URL_checkimage,
+                params: {
+                    "url": encodeURIComponent(CAPTCHA_SERVER_BASE_URL),
+                    "callback": "JSON_CALLBACK"
+                }
             }).then(sucess, error);
+        },
+        txtToCaptcha: function (txt) {
+            console.log('txtToCaptcha(', txt, ')');
+            var canvas = document.createElement("canvas");
+            canvas.setAttribute('width', 120);
+            canvas.setAttribute('height', 38);
+            canvas.setAttribute('id', 'captcha_canvas');
+
+            var context = canvas.getContext('2d');
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.font = 'italic 25px monospace';
+            context.textBaseline = 'top';
+            context.textAlign = 'start';
+            canvas.style.display = 'none';
+
+            var txtArray = txt.split('');
+            var lens = txtArray.length;
+            var color_ = ["#ff0000", "#00ff00", "#333333", "#0000ff"];
+            var height_ = canvas.height - 30;
+            var height_arr = [0, height_, 0, height_];
+            for (var i = 0; i < lens; i++) {
+                context.strokeStyle = color_[i];
+                if (i % 2 == 0) {
+                    context.strokeText(txtArray[i].toUpperCase(), canvas.width * (i) / 4, height_arr[i], canvas.width * (i + 1));
+                } else {
+                    context.strokeText(txtArray[i], canvas.width * (i) / 4, height_arr[i], canvas.width * (i + 1));
+                }
+            }
+            $("#check_img").prop("src", canvas.toDataURL("image/png"));
+        },
+        login: function (cred, sucess, error) {
+            console.log('cred: ', cred);
+            var shit = {
+                name: cred.username,
+                password: cred.password,
+                checkcode: cred.checkCode.toLowerCase()
+            };
+            console.log(JSON.stringify(shit));
+            var arg = encodeURIComponent("/mobile/user/login?jsonInput=" + JSON.stringify(shit));
+            $http({
+                method: "JSONP",
+                url: TEST_SERVER_BASE_URL,
+                params: {
+                    "url": arg,
+                    "callback": "JSON_CALLBACK"
+                }
+            }).then(sucess, error);
+        },
+        setCookie: function (obj) {
+            $cookies.redisName = obj;
         }
     };
 }]);
 //项目服务
 //对相同的API的操作并到一个factory里，可以共用一个常量URL
 App.factory('factoryInvestList', ['$http', '$cacheFactory', function ($http, $cacheFactory) {
-    var TEST_SERVER_BASE_URL = 'http://192.168.1.70/mobile/jsonp?url=';
-    var lru = $cacheFactory('lru', {capacity: 5});//缓存5次请求
+    var TEST_SERVER_BASE_URL = 'http://192.168.1.70/mobile/jsonp';
+    var lru = $cacheFactory('lru', {
+        capacity: 5
+    }); //缓存5次请求
     return {
         //项目列表
         getList: function (sucess, error) {
             var arg = encodeURIComponent("mobile/trade/invest/bill/list?type=0&page=1&num=10");
             $http({
                 method: "JSONP",
-                url: TEST_SERVER_BASE_URL + arg + "&callback=JSON_CALLBACK",
+                url: TEST_SERVER_BASE_URL,
+                params: {
+                    "url": arg,
+                    "callback": "JSON_CALLBACK"
+                },
                 cache: lru
             }).then(sucess, error);
         },
@@ -69,7 +138,11 @@ App.factory('factoryInvestList', ['$http', '$cacheFactory', function ($http, $ca
             console.log('url: ', TEST_SERVER_BASE_URL + arg + "&callback=JSON_CALLBACK");
             $http({
                 method: "JSONP",
-                url: TEST_SERVER_BASE_URL + arg + "&callback=JSON_CALLBACK"
+                url: TEST_SERVER_BASE_URL,
+                params: {
+                    "url": arg,
+                    "callback": "JSON_CALLBACK"
+                }
             }).then(sucess, error);
         }
     };
@@ -86,7 +159,6 @@ App.directive('myDirective', function () {
 //页面标题栏
 App.controller('header_controller', ['$scope', '$http', '$location', '$route', '$routeParams', function ($scope, $http, $location, $route, $routeParams) {
     $scope.getPageTitle = function () {
-        //return $scope.page_titles[$location.path()];
         return $route.current.page_title;
     }
 }]);
@@ -104,15 +176,22 @@ App.controller('main_controller', ['$scope', '$http', '$location', '$route', '$r
 }]);
 //登陆
 App.controller('login_controller', ['$scope', '$http', '$location', '$route', '$routeParams', 'loginService', function ($scope, $http, $location, $route, $routeParams, loginService) {
+    loginService.fetchCaptcha(function (res) {
+        console.log('验证码返回：', res.data);
+        loginService.txtToCaptcha(res.data.checkCode);
+        loginService.setCookie(res.data.redisName);
+    }, function (res) {
+        console.log('err: ', res);
+    });
+
     $scope.login = function () {
         var cred = {
-            "username": $scope.username,
-            "password": $scope.password,
-            "checkcode": $scope.checkcode
+            username: $scope.username,
+            password: $scope.password,
+            checkCode: $scope.checkCode
         };
         loginService.login(cred, function (res) {
-            console.log('sucess & data: ', res.data.data);
-            $scope.item = res.data.data;
+            console.log('success: ', res);
         }, function (res) {
             console.log('err: ', res);
         });
