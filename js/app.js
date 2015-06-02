270d91cf1fd6662fd3596c15652aaf8339923698
陆欢 <ariellushanghai@icloud.com>
2015-06-02 17:13:32 +0800

    完成：个人中心初步

--- js/app.js
+++ js/app.js
@@ -47,7 +47,7 @@ App.run(function ($rootScope) {
 App.factory('userInfo', ['$http', '$cookies', function ($http, $cookies) {
     var user_info = {};
     return {
-        setUserInfo: function(obj) {
+        setUserInfo: function (obj) {
             user_info = obj;
         },
         getUserInfo: function () {
@@ -64,12 +64,12 @@ App.factory('loginService', ['$http', '$cookies', '$location', 'userInfo', funct
     var CAPTCHA_SERVER_BASE_URL = '/verification/checkImage';
     var uid = null;
     var token = null;
-    console.log(userInfo.getUserInfo());
     return {
-        isLoggedIn: function() {
+        isLoggedIn: function () {
             return uid !== null && token !== null;
         },
         fetchCaptcha: function (sucess, error) {
+            var self = this;
             $http({
                 method: "JSONP",
                 url: TEST_SERVER_BASE_URL_checkimage,
@@ -77,7 +77,12 @@ App.factory('loginService', ['$http', '$cookies', '$location', 'userInfo', funct
                     "url": encodeURIComponent(CAPTCHA_SERVER_BASE_URL),
                     "callback": "JSON_CALLBACK"
                 }
-            }).then(sucess, error);
+            }).then(function (res) {
+                self.txtToCaptcha(res.data.checkCode);
+                self.setCookie(res.data.redisName);
+            }, function (res) {
+                console.log('fetchCaptcha() ERR: ', res);
+            });
         },
         txtToCaptcha: function (txt) {
             console.log('txtToCaptcha(', txt, ')');
@@ -109,13 +114,13 @@ App.factory('loginService', ['$http', '$cookies', '$location', 'userInfo', funct
             $("#check_img").prop("src", canvas.toDataURL("image/png"));
         },
         login: function (cred) {
-        //login: function (cred, sucess, error) {
+            //login: function (cred, sucess, error) {
             var shit = {
                 name: cred.username,
                 password: cred.password,
                 checkCode: cred.checkCode.toLowerCase()
             };
-            console.log(JSON.stringify(shit));
+            //console.log(JSON.stringify(shit));
             var arg = encodeURIComponent("/mobile/user/login?jsonInput=" + JSON.stringify(shit));
             $http({
                 method: "JSONP",
@@ -126,22 +131,24 @@ App.factory('loginService', ['$http', '$cookies', '$location', 'userInfo', funct
                     "callback": "JSON_CALLBACK"
                 }
             }).then(function success(res) {
-                console.log("res: ", JSON.stringify(res));
-                if(res.data.result == 0) {
+                //console.log("res: ", JSON.stringify(res));
+                if (res.data.result == 0) {
                     userInfo.setUserInfo(res.data.data);
                     uid = res.data.data.userId;
                     token = res.data.data.token;
                     console.log('userInfo: ', userInfo.getUserInfo());
                     $location.path('/personal_center').replace();
+                } else {
+                    return console.error('怪余祺')
                 }
             }, function error(err) {
-                console.log('Login failed: ',err)
+                console.log('Login failed: ', err)
             });
         },
         setCookie: function (obj) {
             $cookies.redisName = obj;
         },
-        get_uid_token: function() {
+        get_uid_token: function () {
             return {
                 "uid": uid,
                 "token": token
@@ -187,68 +194,74 @@ App.factory('factoryInvestList', ['$http', '$cacheFactory', function ($http, $ca
     };
 }]);
 //个人中心
-App.factory('factoryPersonalCenter', ['$http', '$cacheFactory', 'loginService', function ($http, $cacheFactory, loginService) {
+App.factory('factoryPersonalCenter', ['$http', '$cacheFactory', '$rootScope', 'loginService', function ($http, $cacheFactory, $rootScope, loginService) {
+    var this_service = this;
     //var TEST_SERVER_BASE_URL = 'http://192.168.1.70/mobile/jsonp';
     var TEST_SERVER_BASE_URL = 'http://192.168.1.174/mobile/jsonp';
+    var data = {};
     return {
-        fetchData: function() {
-            console.log('fetchData');
+        fetchData: function () {
+            var self = this;
             var arg = encodeURIComponent("mobile/user/customerAccount");
-            console.log()
-            $http({
+            return $http({
                 method: "JSONP",
                 url: TEST_SERVER_BASE_URL,
                 params: {
                     "url": arg,
                     "callback": "JSON_CALLBACK",
                     "uid": loginService.get_uid_token().uid,
-                    "token":loginService.get_uid_token().token
+                    "token": loginService.get_uid_token().token
                 }
-            }).then(function(res) {
-                console.log('sucess: ',res);
-            }, function(res) {
-                console.log('err: ',res);
+            }).then(function (res) {
+                if (res.data.result == 0) {
+                    console.log('factoryPersonalCenter.fetchData() sucess: ', res);
+                    self.setData(res.data.data);
+                    $rootScope.$broadcast('dataFetched');
+                    //return self.getData();
+                    return res.data.data;
+                } else {
+                    console.error('怪余祺');
+                    return {};
+                }
+            }, function (res) {
+                console.log('err: ', res);
+                return {};
             });
+        },
+        getData: function () {
+            console.log('factoryPersonalCenter.getData(', data, ')');
+            return data;
+        },
+        setData: function (obj) {
+            console.log('factoryPersonalCenter.setData(', obj, ')');
+            return data = obj;
         }
     }
 }]);
-//指令
-App.directive('myDirective', function () {
-    return {
-        restrict: "A",
-        replace: "true",
-        templateUrl: "partials/myDirective.html"
-    };
-});
+
 //页面标题栏
-App.controller('header_controller', ['$scope', '$http', '$location', '$route', '$routeParams', function ($scope, $http, $location, $route, $routeParams) {
+App.controller('header_controller', ['$scope', '$route', function ($scope, $route) {
     $scope.getPageTitle = function () {
         return $route.current.page_title;
     }
 }]);
 //底部导航条
-App.controller('menu_controller', ['$scope', '$http', '$location', '$route', '$routeParams', function ($scope, $http, $location, $route, $routeParams) {
+App.controller('menu_controller', ['$scope', '$location', function ($scope, $location) {
     $scope.isActive = function (view_location) {
         return view_location === $location.path();
     };
 }]);
 //首页
-App.controller('main_controller', ['$scope', '$http', '$location', '$route', '$routeParams', function ($scope, $http, $location, $route, $routeParams) {
-    $scope.$route = $route;
-    $scope.$location = $location;
-    $scope.$routeParams = $routeParams;
+App.controller('main_controller', ['$scope', function ($scope) {
+
 }]);
 //登陆
-App.controller('login_controller', ['$scope', '$http', '$location', '$route', '$routeParams', 'loginService', function ($scope, $http, $location, $route, $routeParams, loginService) {
+App.controller('login_controller', ['$scope', 'loginService', function ($scope, loginService) {
+    $scope.username = '13812345678';
+    $scope.password = '111111';
 
-    $scope.fetchCaptcha = function() {
-        loginService.fetchCaptcha(function (res) {
-            console.log('验证码返回：', res.data);
-            loginService.txtToCaptcha(res.data.checkCode);
-            loginService.setCookie(res.data.redisName);
-        }, function (res) {
-            console.log('err: ', res);
-        });
+    $scope.fetchCaptcha = function () {
+        loginService.fetchCaptcha();
     };
 
     $scope.fetchCaptcha();
@@ -259,18 +272,15 @@ App.controller('login_controller', ['$scope', '$http', '$location', '$route', '$
             password: $scope.password,
             checkCode: $scope.checkCode
         };
-        //loginService.login(cred, function (res) {
-        //    console.log('success: ', res);
-        //}, function (res) {
-        //    console.log('err: ', res);
-        //});
         loginService.login(cred);
     }
 }]);
 //更多
-App.controller('more_controller', ['$scope', '$http', '$location', '$route', '$routeParams', function ($scope, $http, $location, $route, $routeParams) {}]);
+App.controller('more_controller', ['$scope', 'factoryMore', function ($scope, factoryMore) {
+
+}]);
 //投资列表
-App.controller('invest_controller', ['$scope', '$http', '$location', '$route', '$routeParams', 'factoryInvestList', function ($scope, $http, $location, $route, $routeParams, factoryInvestList) {
+App.controller('invest_controller', ['$scope', 'factoryInvestList', function ($scope, factoryInvestList) {
     $scope.billTypeList = ['待购买', '银票纯', '银票红', '银商', '转让', '第三方平台'];
     factoryInvestList.getList(function (res) {
         console.log('sucess: ', res.data.data);
@@ -280,7 +290,7 @@ App.controller('invest_controller', ['$scope', '$http', '$location', '$route', '
     });
 }]);
 //项目详情
-App.controller('invest_item_detail_controller', ['$scope', '$http', '$location', '$route', '$routeParams', 'factoryInvestList', function ($scope, $http, $location, $route, $routeParams, factoryInvestList) {
+App.controller('invest_item_detail_controller', ['$scope', '$route', '$routeParams', 'factoryInvestList', function ($scope, $route, $routeParams, factoryInvestList) {
     var billId = $routeParams.id.replace(':', '');
     factoryInvestList.getDetail(billId, function (res) {
         console.log('sucess & data: ', res.data.data);
@@ -290,11 +300,14 @@ App.controller('invest_item_detail_controller', ['$scope', '$http', '$location',
     });
 }]);
 //个人中心
-App.controller('personal_center_controller', ['$scope', '$http', '$location', '$route', '$routeParams', 'userInfo', 'factoryPersonalCenter' , function ($scope, $http, $location, $route, $routeParams, userInfo, factoryPersonalCenter) {
-    $scope.fetchData = function() {
-        factoryPersonalCenter.fetchData();
-    };
-    $scope.fetchData();
+App.controller('personal_center_controller', ['$scope', 'userInfo', 'factoryPersonalCenter', function ($scope, userInfo, factoryPersonalCenter) {
+    $scope.$on('dataFetched', function () {
+        //console.log('get brocatst: dataFetched');
+        $scope.data = factoryPersonalCenter.getData();
+    });
+    factoryPersonalCenter.fetchData();
 }]);
 //首页
-App.controller('index_page_controller', ['$scope', '$http', '$location', '$route', '$routeParams', function ($scope, $http, $location, $route, $routeParams) {}]);
+App.controller('index_page_controller', ['$scope', '$http', '$location', '$route', '$routeParams', function ($scope, $http, $location, $route, $routeParams) {
+
+}]);
